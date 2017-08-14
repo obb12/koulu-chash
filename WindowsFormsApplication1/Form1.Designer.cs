b3bdccf907fe0468e789f62d5e96440c75012d9d
Rajala Roger <s1500593@student.businesscollege.fi>
2017-08-14 13:34:29 +0300

    testi

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -121,7 +121,7 @@
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
             this.Name = "Form1";
-            this.Text = "s";
+            this.Text = "CryptoStalk";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
