c10fcb9ad7ab46d274aeeda8db64013c14ead1dc
omar <omar.bb@hotmail.fi>
2017-08-14 13:37:20 +0300

    JSON

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -33,8 +33,31 @@ namespace WindowsFormsApplication1
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            WebClient webClient = new WebClient();
-            dynamic result = webClient.DownloadString("https://api.foursquare.com/v2/users/self?oauth_token=XXXXXXX");
+            // create a new instance of WebClient
+            WebClient client = new WebClient();
+
+            // set the user agent to IE6
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.0.3705;)");
+            try
+            {
+
+                // actually execute the GET request
+                string ret = client.DownloadString("https://api.coindesk.com/v1/bpi/currentprice.json");
+
+                // ret now contains the contents of the webpage
+                Console.WriteLine("First 256 bytes of response: " + ret.Substring(0, 265));
+
+            }
+            catch (WebException we)
+            {
+                // WebException.Status holds useful information
+                Console.WriteLine(we.Message + "\n" + we.Status.ToString());
+            }
+            catch (NotSupportedException ne)
+            {
+                // other errors
+                Console.WriteLine(ne.Message);
+            }
         }
     }
 }
