452bc092478c90b6fcaca29ee5684d9718c9f65a
omar <omar.bb@hotmail.fi>
2017-08-15 09:22:06 +0300

    labeliin lisäsin tekstin

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -6,9 +6,10 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Net;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Newtonsoft.Json;
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
@@ -33,31 +34,15 @@ namespace WindowsFormsApplication1
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            // create a new instance of WebClient
-            WebClient client = new WebClient();
+            var client = new WebClient();
+            var stream = client.OpenRead("https://api.coindesk.com/v1/bpi/currentprice.json");
+            var reader = new StreamReader(stream);
+            
+                var jObject = Newtonsoft.Json.Linq.JObject.Parse(reader.ReadLine());
+                Console.WriteLine(jObject["bpi"]["EUR"]["rate_float"]);
+                label1.Text = (string) jObject["bpi"]["EUR"]["rate_float"];
+            
 
-            // set the user agent to IE6
-            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.0.3705;)");
-            try
-            {
-
-                // actually execute the GET request
-                string ret = client.DownloadString("https://api.coindesk.com/v1/bpi/currentprice.json");
-
-                // ret now contains the contents of the webpage
-                Console.WriteLine("First 256 bytes of response: " + ret.Substring(0, 265));
-
-            }
-            catch (WebException we)
-            {
-                // WebException.Status holds useful information
-                Console.WriteLine(we.Message + "\n" + we.Status.ToString());
-            }
-            catch (NotSupportedException ne)
-            {
-                // other errors
-                Console.WriteLine(ne.Message);
-            }
-        }
+        } 
     }
 }
