b39dd0ddd7df6b3f9eb2ad5d749fb86c0632004e
Rajala Roger <s1500593@student.businesscollege.fi>
2017-08-21 13:16:29 +0300

    hello

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -28,101 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioBTC = new System.Windows.Forms.RadioButton();
+            this.radioETH = new System.Windows.Forms.RadioButton();
+            this.radioXRP = new System.Windows.Forms.RadioButton();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.radioButton5 = new System.Windows.Forms.RadioButton();
-            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // radioButton1
+            // radioBTC
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(13, 13);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(58, 17);
-            this.radioButton1.TabIndex = 1;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "BitCoin";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            this.radioBTC.AutoSize = true;
+            this.radioBTC.BackColor = System.Drawing.Color.Transparent;
+            this.radioBTC.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioBTC.ForeColor = System.Drawing.Color.White;
+            this.radioBTC.Location = new System.Drawing.Point(13, 13);
+            this.radioBTC.Name = "radioBTC";
+            this.radioBTC.Size = new System.Drawing.Size(65, 17);
+            this.radioBTC.TabIndex = 1;
+            this.radioBTC.TabStop = true;
+            this.radioBTC.Text = "BitCoin";
+            this.radioBTC.UseVisualStyleBackColor = false;
+            this.radioBTC.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
-            // radioButton2
+            // radioETH
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(77, 13);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(70, 17);
-            this.radioButton2.TabIndex = 2;
-            this.radioButton2.Text = "Ethereum";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioETH.AutoSize = true;
+            this.radioETH.BackColor = System.Drawing.Color.Transparent;
+            this.radioETH.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioETH.ForeColor = System.Drawing.Color.White;
+            this.radioETH.Location = new System.Drawing.Point(77, 13);
+            this.radioETH.Name = "radioETH";
+            this.radioETH.Size = new System.Drawing.Size(78, 17);
+            this.radioETH.TabIndex = 2;
+            this.radioETH.Text = "Ethereum";
+            this.radioETH.UseVisualStyleBackColor = false;
+            this.radioETH.CheckedChanged += new System.EventHandler(this.radioETH_CheckedChanged);
             // 
-            // radioButton3
+            // radioXRP
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(153, 13);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(47, 17);
-            this.radioButton3.TabIndex = 3;
-            this.radioButton3.Text = "XRP";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioXRP.AutoSize = true;
+            this.radioXRP.BackColor = System.Drawing.Color.Transparent;
+            this.radioXRP.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioXRP.ForeColor = System.Drawing.Color.White;
+            this.radioXRP.Location = new System.Drawing.Point(153, 13);
+            this.radioXRP.Name = "radioXRP";
+            this.radioXRP.Size = new System.Drawing.Size(50, 17);
+            this.radioXRP.TabIndex = 3;
+            this.radioXRP.Text = "XRP";
+            this.radioXRP.UseVisualStyleBackColor = false;
+            this.radioXRP.CheckedChanged += new System.EventHandler(this.radioXRP_CheckedChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 59);
+            this.textBox1.Location = new System.Drawing.Point(13, 36);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(233, 101);
+            this.textBox1.Size = new System.Drawing.Size(233, 23);
             this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // radioButton4
+            // button1
             // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(153, 36);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(31, 17);
-            this.radioButton4.TabIndex = 5;
-            this.radioButton4.Text = "$";
-            this.radioButton4.UseVisualStyleBackColor = true;
-            // 
-            // radioButton5
-            // 
-            this.radioButton5.AutoSize = true;
-            this.radioButton5.Location = new System.Drawing.Point(77, 36);
-            this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(31, 17);
-            this.radioButton5.TabIndex = 6;
-            this.radioButton5.Text = "€";
-            this.radioButton5.UseVisualStyleBackColor = true;
-            // 
-            // radioButton6
-            // 
-            this.radioButton6.AutoSize = true;
-            this.radioButton6.Location = new System.Drawing.Point(13, 36);
-            this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(31, 17);
-            this.radioButton6.TabIndex = 7;
-            this.radioButton6.Text = "£";
-            this.radioButton6.UseVisualStyleBackColor = true;
-            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.Location = new System.Drawing.Point(201, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 24);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(258, 172);
-            this.Controls.Add(this.radioButton6);
-            this.Controls.Add(this.radioButton5);
-            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.radioButton3);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.radioXRP);
+            this.Controls.Add(this.radioETH);
+            this.Controls.Add(this.radioBTC);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Form1";
             this.Text = "CryptoStalk";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,13 +126,11 @@
 
         #endregion
 
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioBTC;
+        private System.Windows.Forms.RadioButton radioETH;
+        private System.Windows.Forms.RadioButton radioXRP;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.RadioButton radioButton5;
-        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.Button button1;
 
     }
 }
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -5,6 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
 
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,29 +21,181 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
             FormBorderStyle = FormBorderStyle.None;
+            button1.Text = "Exit";
+            
             
         }
 
-
+        public Timer timer1;
+        public List<Timer> Timerlist;
+        public Timer timer2;
+        public Timer timer3;
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
+        
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            WebClient webClient = new WebClient();
-            dynamic result = webClient.DownloadString("https://api.foursquare.com/v2/users/self?oauth_token=XXXXXXX");
+            var client = new WebClient();
+            var stream = client.OpenRead("https://api.coindesk.com/v1/bpi/currentprice.json");
+            var reader = new StreamReader(stream);
+
+            var jObject = Newtonsoft.Json.Linq.JObject.Parse(reader.ReadLine());
+            Console.WriteLine(jObject["bpi"]["USD"]["rate_float"]);
+            textBox1.Text = "1 USD = " + (string)jObject["bpi"]["USD"]["rate_float"] + " BTC";
+
+
+            if (timer1 != null )
+            {
+                if (!radioBTC.Checked)
+                {
+                    timer1.Stop();
+                }
+            }
+            else
+            {
+                Timerlist = new List<Timer>();
+                timer1 = new Timer();
+                timer1.Tick += new EventHandler(timer1_Tick);
+                timer1.Interval = 10000; // in miliseconds
+                timer1.Start();
+                Console.WriteLine(this);
+            }
+        }
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            Console.WriteLine("hi");
+            if (radioBTC.Checked)
+            {
+                var client = new WebClient();
+                var stream = client.OpenRead("https://api.coindesk.com/v1/bpi/currentprice.json");
+                var reader = new StreamReader(stream);
+
+                var jObject = Newtonsoft.Json.Linq.JObject.Parse(reader.ReadLine());
+                Console.WriteLine(jObject["bpi"]["USD"]["rate_float"]);
+                textBox1.Text = "1 USD = " + (string)jObject["bpi"]["USD"]["rate_float"] + " BTC";
+            }
+            
         }
 
         private void radioButton6_CheckedChanged(object sender, EventArgs e)
         {
 
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void off(object sender, EventArgs e) { }
+        public void timer2_Tick(object sender, EventArgs e)
+        {
+
+            var client = new WebClient();
+            var stream = client.OpenRead("http://data.ripple.com/v2/exchange_rates/XRP/USD+rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B");
+            var reader = new StreamReader(stream);
+
+            var jObject = Newtonsoft.Json.Linq.JObject.Parse(reader.ReadLine());
+            textBox1.Text = "1 USD = " + (string)jObject["rate"] + " XRP";
+        }
+        private void radioXRP_CheckedChanged(object sender, EventArgs e)
+        
+        {
+
+            var client = new WebClient();
+            var stream = client.OpenRead("http://data.ripple.com/v2/exchange_rates/XRP/USD+rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B");
+            var reader = new StreamReader(stream);
+
+            var jObject = Newtonsoft.Json.Linq.JObject.Parse(reader.ReadLine());
+           // Console.WriteLine(jObject["bpi"]["EUR"]["rate_float"]);
+            textBox1.Text = "1 USD = " + (string)jObject["rate"] + " XRP";
+
+            if (timer2 != null)
+            {
+                if (!radioXRP.Checked)
+                {
+                    timer2.Stop();
+                }
+            }
+            else
+            {
+                Timerlist = new List<Timer>();
+                timer2 = new Timer();
+                timer2.Tick += new EventHandler(timer2_Tick);
+                timer2.Interval = 10000; // in miliseconds
+                timer2.Start();
+                Console.WriteLine(this);
+            }
+        }
+        public void timer3_Tick(object sender, EventArgs e)
+        {
+            var client = new WebClient();
+            var stream = client.OpenRead("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD");
+            var reader = new StreamReader(stream);
+
+            var jObject = Newtonsoft.Json.Linq.JObject.Parse(reader.ReadLine());
+            // Console.WriteLine(jObject["bpi"]["EUR"]["rate_float"]);
+            textBox1.Text = "1 USD = " + (string)jObject["USD"] + " ETH";
+        }
+        private void radioETH_CheckedChanged(object sender, EventArgs e)
+        {
+            var client = new WebClient();
+            var stream = client.OpenRead("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD");
+            var reader = new StreamReader(stream);
+
+            var jObject = Newtonsoft.Json.Linq.JObject.Parse(reader.ReadLine());
+            // Console.WriteLine(jObject["bpi"]["EUR"]["rate_float"]);
+            textBox1.Text = "1 USD = " + (string)jObject["USD"] + " ETH";
+
+            if (timer3 != null)
+            {
+                if (!radioETH.Checked)
+                {
+                    timer3.Stop();
+                }
+            }
+            else
+            {
+                Timerlist = new List<Timer>();
+                timer3 = new Timer();
+                timer3.Tick += new EventHandler(timer3_Tick);
+                timer3.Interval = 10000; // in miliseconds
+                timer3.Start();
+                Console.WriteLine(this);
+            }
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            
+        }
+        private bool mouseDown;
+        private Point lastLocation;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
     }
 }
